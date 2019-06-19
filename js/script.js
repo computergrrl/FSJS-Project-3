60949763810566924e8eedd61205835cac5dea71
Amber Stevens <anani.mowse@gmail.com>
2019-06-19 14:56:37 -0700

    Why won't this work? Hmmm

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
-//
-// const $inputName = $('#name');
-// $inputName.focus();
-//
-// const $jobTitle = $('#title');
-//
-// $jobTitle.on('change' , function(e){
-//
-//
-//
-// });
+
+const $inputName = $('#name');
+
+//auto focus on name field upon page load
+$inputName.focus();
+
+const $jobTitle = $('#title');
+
+//event listener to add text box if "other" is selected in dropdown menu
+$jobTitle.on('change' , function(){
+
+    const $selected = $('#title option:selected').val();
+
+    console.log($selected);
+
+    if ($selected === 'other') {
+
+       //console.log('You selected "Other!"')
+
+    $jobTitle.append('<input type="text">');
+
+
+    }
+
+
+
+});
