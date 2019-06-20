8aad95d0b05166787afdfc865f7cd22920739719
Amber Stevens <anani.mowse@gmail.com>
2019-06-19 21:47:32 -0700

    Created shirt vars for dropdown but they're cons-logging as "undefined"?

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 
+/***************************Job Role Section***************************/
+
 const $inputName = $('#name');
 const $jobRole = $('#other-title');
+
+
 
 //hide other job Role input field
 $jobRole.hide();
@@ -14,14 +18,33 @@ const $jobTitle = $('#title');
 //event listener to add text box if "other" is selected in dropdown menu
 $jobTitle.on('change' , function(){
 
+          //set a variable to the value of option selected in from dropdown
           const $selected = $('#title option:selected').val();
 
+          //if "other" is selected then show hidden field
           if ($selected === 'other') {
 
           $jobRole.show();
 
+         //if any other option is selected hide the extra text field
       }  else {
           $jobRole.hide();
       }
 
 });
+
+/***********************T-Shirt Info Section ****************************/
+
+const $shirtTheme = $('#design');
+const $shirtColors = $('#colors-js-puns');
+
+
+//event listener to detect change in the dropdown menu
+$shirtTheme.on('change' , function(){
+
+
+          //set a variable to the value of option selected in from dropdown
+          const $theme = $('#shirtTheme option:selected').val();
+
+              console.log($theme);
+          });
