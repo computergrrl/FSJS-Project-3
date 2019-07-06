092718550a336a64f5f7d597344bbdde5017f21a
Amber Stevens <anani.mowse@gmail.com>
2019-07-06 16:32:27 -0700

    Changed logic so all error mssgs show at once on attempted submit

--- js/formSubmission.js
+++ js/formSubmission.js
@@ -8,62 +8,49 @@ const $registerButton = $('button');
 $registerButton.click(function(e){
 
     //checks at least one checkbox is checked
-    if ($('#totalCost').text() === "") {
+    if (
+    $('#totalCost').text() === "" ||
+    $inputName.val() === "" ||
+    $email.val() === "" ||
+    !emailValidate($email.val())) {
         e.preventDefault();
-        $activities.focus();
-        $activitiesWarning.show();
+
+          if($('#totalCost').text() === "") {
+          $activitiesWarning.show();
 
     //Makes sure name field isn't blank
-    }  else if($inputName.val() === "") {
-       e.preventDefault();
-       $inputName.focus();
-       $nameWarning.show();
+          }   if($inputName.val() === "") {
+              $nameWarning.show();
 
     //Makes sure email field isn't blank
-    }  else if($email.val() === "") {
-       e.preventDefault();
-       $email.focus();
-       $emailWarning2.show();
+          }  if($email.val() === "") {
+             $emailWarning2.show();
 
     //makes sure email is formatted correctly
-    }  else if(!emailValidate($email.val())) {
-       e.preventDefault();
-       $email.focus();
-       $emailWarning.show();
-
+          }  if(!emailValidate($email.val())) {
+             $emailWarning.show();
+          }
     }
 
        /* Following functions check that credit card, zip and CVV fields aren't empty and that all have been formatted correctly */
-       else if ($payment.val() === 'credit card') {
+        if ($payment.val() === 'credit card') {
 
             if($ccInput.val() === "") {
-              e.preventDefault();
-              $ccInput.focus();
               $ccWarning2.show();
 
-          }  else if(!ccValidate($ccInput.val())) {
-             e.preventDefault();
-             $ccInput.focus();
+          } if(!ccValidate($ccInput.val())) {
              $ccWarning.show();
 
-          }  else if($zipInput.val() === "") {
-             e.preventDefault();
-             $zipInput.focus();
+          }  if($zipInput.val() === "") {
              $zipWarning2.show();
 
-          }  else if(!zipValidate($zipInput.val())) {
-             e.preventDefault();
-             $zipInput.focus();
+          }  if(!zipValidate($zipInput.val())) {
              $zipWarning.show();
 
-          }  else if($cvvInput.val() === "") {
-             e.preventDefault();
-             $cvvInput.focus();
+          }  if($cvvInput.val() === "") {
              $cvvWarning2.show();
 
-          }  else if(!cvvValidate($cvvInput.val())) {
-             e.preventDefault();
-             $cvvInput.focus();
+          }  if(!cvvValidate($cvvInput.val())) {
              $cvvWarning.show();
 
           }
